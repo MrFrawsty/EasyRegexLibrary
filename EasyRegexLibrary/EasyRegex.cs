faeb083f77ea59ac00a9abce533722355546b3d1
Kenny Carneal <kennyjoec@gmail.com>
2021-08-04 10:36:49 -0800

    Changed name of class from EasyRegexCollections to EasyRegex

--- EasyRegexLibrary/EasyRegex.cs
+++ EasyRegexLibrary/EasyRegex.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EasyRegexLibrary
 {
-  
-     public static class EasyRegexCollections
+
+    public static class EasyRegex
      {  
         public static MatchCollection FindCreditCardNumbers(string text)
         {
